beb26dcb508dacb25648c9199e55219e4dda8605
mikan19 <mellamo.rina.ty@gmail.com>
2023-04-23 22:28:17 +0900

    fix:ガード節を修正しました

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,42 @@
 <?php
-echo 'Welcome TECH QUEST!';
+$word = "fuga";
+for ($i = 1; $i <= 6; $i++) {
+    if ($word == "hoge" && $i % 2 == 0) {
+        echo "wordがhoge かつ " . $i . "は2の倍数です" . "\n";
+        continue;
+    }
+
+    if($word == "hoge" && $i % 2 == 1){
+        echo "wordがhoge かつ " . $i . "は2の倍数以外です" . "\n";
+        continue;
+    }
+    
+    if ($i % 2 == 0) {
+        echo "wordがhogeでない かつ " . $i . "は2の倍数です" . "\n";
+        continue;
+    }
+
+    if($i % 2 == 1){
+        echo "wordがhogeでない かつ " . $i . "は2の倍数以外です" . "\n";
+        continue;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
